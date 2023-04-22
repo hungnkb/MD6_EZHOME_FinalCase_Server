aa008d4447c13da90fff1654e6b9a68fc8e60e43
PhuocMai31 <Phuwowsc@gmail.com>
2023-04-22 16:22:36 +0700

    pull Comment

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -75,6 +75,7 @@ export class UserService {
 
   async update(body: UpdateUserDto): Promise<any> {
     let { email, phone, fullName, address, role } = body;
+    console.log(body);
 
     let user = await this.userRepository.findOne({ where: { email } });
     if (!user) {
@@ -119,6 +120,7 @@ export class UserService {
   }
 
   async sendLinkForgotPassword(body): Promise<any> {
+    console.log(body);
     if (!body.email) {
       throw new HttpException('INsert your email', HttpStatus.BAD_REQUEST);
     } else {
@@ -146,8 +148,8 @@ export class UserService {
           .set({ password: hashPassword })
           .where({ email })
           .execute();
+        throw new HttpException('Reset password success', HttpStatus.OK);
       }
-      throw new HttpException('Reset password success', HttpStatus.OK);
     });
   }
 
