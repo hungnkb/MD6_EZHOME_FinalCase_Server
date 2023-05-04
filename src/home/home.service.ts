02b130f0d3d8f3a3a1f48f5b03fb649075db9d9d
hungnkb <hungnkb@gmail.com>
2023-05-04 19:00:38 +0700

    pull

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -7,6 +7,7 @@ import { HomeSchema } from './entities/home.entity';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 import { HomeImageSchema } from './entities/homeImage.entity';
 import { CouponService } from 'src/coupon/coupon.service';
+import { plainToClass, classToPlain } from 'class-transformer';
 
 @Injectable()
 export class HomeService {
@@ -18,7 +19,7 @@ export class HomeService {
     private userService: UserService,
     private cloudinaryService: CloudinaryService,
     private couponService: CouponService,
-  ) {}
+  ) { }
 
   async create(body: CreateHomeDto): Promise<Object> {
     const {
@@ -317,16 +318,20 @@ export class HomeService {
       .getMany();
   }
 
-  async patch({idCoupon, idHome}): Promise<any> {
-    console.log(idCoupon);
-    
-    const coupon = await this.couponService.findByKeyword(idCoupon);
+  async patch(body: any): Promise<any> {
+    const coupon = await this.couponService.findByKeyword(body);
+    const couponObj = classToPlain(coupon);
+
     const dateNow = new Date();
     const now = dateNow.getTime();
-    // const startDate = new Date(coupon.startDate)
-
-    console.log(coupon);
-    
-    // return this.homeRepository.update({idHome: body.idHome}, {idCoupon: body.idCoupon});
+    const startDate = new Date(couponObj.startDate);
+    const start = startDate.getTime();
+    const endDate = new Date(couponObj.endDate);
+    const end = endDate.getTime();
+    if (now > end) {
+      throw new HttpException('Coupon is expired', HttpStatus.BAD_REQUEST,);
+    }
+    await this.homeRepository.update({ idHome: body.idHome }, { idCoupon: body.idCoupon });
+    return HttpStatus.OK;
   }
 }
