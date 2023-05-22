91e0402229fe13025a447fe2d5b2059eef6adcfa
hungnkb <hungnkb@gmail.com>
2023-05-22 16:37:27 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import 'reflect-metadata';
 import { SocketAdapter } from './shared/ulti/socket/SocketAdapter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+const config = new DocumentBuilder()
+  .setTitle('EZHOME')
+  .setDescription('The EZHOME API description')
+  .setVersion('1.0')
+  .addTag('ezhome')
+  .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const configService = new ConfigService();
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
